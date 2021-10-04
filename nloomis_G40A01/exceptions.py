2a9f11953a2404b1ca5bdb122d3d6b15e38183e8
nloomis <noahmmLoomis@gmail.com>
2021-10-04 08:29:57 -0400

    Stash later

--- nloomis_G40A01/exceptions.py
+++ nloomis_G40A01/exceptions.py
@@ -1,2 +1,8 @@
 class TaskNotFoundException(BaseException):
     pass
+
+class InvalidActionException(BaseException):
+    pass
+
+class InvalidListTypeException(BaseException):
+    pass
--- nloomis_G40A01/todo.py
+++ nloomis_G40A01/todo.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import exceptions as e
 
 
@@ -21,6 +22,14 @@ class Task():
 
     def __str__(self):
         return f'{self.desc} {self.priority} {self.project}'
+    
+    def upd_desc(self, new_desc):
+        self.desc = new_desc
+
+
+def ensure_created_file():
+    if not os.path.isfile('tasks.txt'):
+        open('tasks.txt', 'w').close()
 
 
 def get_new_task_id():
@@ -29,10 +38,10 @@ def get_new_task_id():
     return int(list(task_list)[-1]) + 1
 
 
-def val_action(action):
-    if action == "add" or action == "udp" or action == "rem" or action == "done" or action == "list" or action == "purge":
-        return action
-    raise Exception("Bad action, must be add, udp, rem, done, list or purge")
+def get_task_id(task_id):
+    if task_id in task_list:
+        return task_id
+    raise e.TaskNotFoundException(f'No task was found with task_id: {task_id}')
 
 
 def get_priority(task):
@@ -63,42 +72,57 @@ def get_project(task):
     return None
 
 
-def add_to_list(str):
-    task_list[id] = Task(get_new_task_id(), get_desc(str), get_priority(str), get_project(str))
+def get_todo_sorted_by_priority():
+    return sorted({key: task_list[key] for key in task_list if task_list[key].completed == 'False'}.items(), key=lambda s: s[1].priority)
 
 
-def get_task_id(task_str):
-    return int(task_str[0])
+def val_action(action):
+    if action == "add" or action == "udp" or action == "rem" or action == "done" or action == "list" or action == "purge":
+        return action
+    raise e.InvalidActionException(
+        "Invalid action. Action can be add, udp, rem, done, list or purge")
+
+
+def val_list_type(list_type):
+    if len(list_type) < 1 or list_type != "all" or list_type != "todo":
+        raise e.InvalidListTypeException(
+            "Can only list of type 'all' or 'todo'")
+
 
 def print_header():
-    print(f'\n{"Completed":^10}|{"Description":^45}|{"Priority":^15}|{"Project":^15}|')
+    print(
+        f'\n{"Completed":^10}|{"Description":^45}|{"Priority":^15}|{"Project":^15}|')
     print("-----------------------------------------------------------------------------------------")
+
 
 def print_all():
     for key in task_list:
-        print(f'{task_list[key].completed:^10}|{task_list[key].desc:^45}|{task_list[key].priority:^15}|{task_list[key].project:^15}|')
+        print(
+            f'{task_list[key].completed:^10}|{task_list[key].desc:^45}|{task_list[key].priority:^15}|{task_list[key].project:^15}|')
+
 
 def print_todo():
     for task in get_todo_sorted_by_priority():
-        print(f'{task[1].completed:^10}|{task[1].desc:^45}|{task[1].priority:^15}|{task[1].project:^15}|')
+        print(
+            f'{task[1].completed:^10}|{task[1].desc:^45}|{task[1].priority:^15}|{task[1].project:^15}|')
 
 
-def get_task_id(task_id):
-    if task_id in task_list:
-        return task_id
-    raise e.TaskNotFoundException(f'No task was found with task_id: {task_id}')
+def add_task(str):
+    task_list[id] = Task(get_new_task_id(), get_desc(
+        str), get_priority(str), get_project(str))
 
-# TODO Delete the task from the task_list
+
 def rem_task(task_id):
     try:
         found_task_id = get_task_id(task_id)
         del task_list[found_task_id]
+        print(f'Task removed with the task_id of {task_id}')
     except e.TaskNotFoundException as msg:
         print(msg)
         
-        
-def get_todo_sorted_by_priority():
-    return sorted({key: task_list[key] for key in task_list if task_list[key].completed == 'False'}.items(), key=lambda s: s[1].priority)
+def upd_task(task_id, new_desc):
+    
+
 
 def write_to_file():
     out_file = open('tasks.txt', 'w')
@@ -123,6 +147,7 @@ def init_task_list():
         tasks[id] = Task(id, desc, priority, project, completed=comp)
     return tasks
 
+
 def change_completion_status(task_id):
     try:
         found_task_id = get_task_id(task_id)
@@ -131,36 +156,42 @@ def change_completion_status(task_id):
         print(msg)
 
 
-
 def prompt_add():
     print("Prompt for all the needed info")
 
+
 if __name__ == "__main__":
+    ensure_created_file()
     task_list = init_task_list()
-    print("""
-          ------------TODO LIST------------
-          """)
+    print("\n------------TODO LIST------------\n")
     while True:
-        line = input("Please enter a command to start: ").split()
-        action = val_action(line[0].strip()).lower()
-        if action == "add":
-            if len(line) > 1:
-                add_to_list(line[1:])
-            else:
-                prompt_add()
-        elif action == "list":
-            if line[1].strip().lower() == 'all':
+        try:
+            line = input("Please enter a command to start: ").split()
+            action = val_action(line[0].strip()).lower()
+
+            if action == "add":
+                if len(line) > 1:
+                    add_task(line[1:])
+                else:
+                    prompt_add()
+            elif action == "list":
+                val_list_type(line[1:])
                 print_header()
-                print_all()
-            elif line[1].strip().lower() == 'todo':
-                print_header()
-                print_todo()
-            else:
-                raise Exception("Can only list of type 'all' or 'todo'")
-        elif action == 'done':
-            if len(line) > 1:
-                change_completion_status(line[1])
-                break
-        elif action == 'rem':
-            rem_task(line[1])
-    write_to_file()
+                if line[1].strip().lower() == 'all':
+                    print_all()
+                elif line[1].strip().lower() == 'todo':
+                    print_todo()
+            elif action == 'done':
+                if len(line) > 1:
+                    change_completion_status(line[1])
+            elif action == 'rem':
+                rem_task(line[1])
+            elif action == 'upd':
+                upd_task(line[1], line[2:])
+
+        except KeyboardInterrupt:
+            write_to_file()
+            print("\nExiting to-do list...")
+            break
+        except (e.InvalidActionException, e.InvalidListTypeException) as msg:
+            print(msg)
